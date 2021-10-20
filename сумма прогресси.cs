40159b5b4378a35fcd507e32c94b4c359c30d304
daniilkhmelnitsky <90950125+daniilkhmelnitsky@users.noreply.github.com>
2021-10-20 13:27:38 +0300

    Add files via upload

---
+++ сумма прогресси.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace arif.sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i, n, x = 0;
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i < n; i++)
+            {
+                x = x + i;
+            }
+            Console.WriteLine(x);
+        }
+    }
+}
