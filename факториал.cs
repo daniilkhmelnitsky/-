d67a522f90056e9890eeea1d5af42ba3a7c13751
daniilkhmelnitsky <90950125+daniilkhmelnitsky@users.noreply.github.com>
2021-10-21 17:13:25 +0300

    Add files via upload

---
+++ факториал.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace sale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, d=1, i = 1;
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a > 0)
+            {
+                do
+                {
+                    d *= i;
+                    i = i + 1;
+                }
+                while (i < a+1);
+            }
+            Console.WriteLine(d);
+
+        }
+    }
+}
