bca1337d258d9f7c5e9029e6a80b1d5508f27707
daniilkhmelnitsky <90950125+daniilkhmelnitsky@users.noreply.github.com>
2021-11-17 13:16:39 +0300

    Add files via upload

---
+++ полиндром.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s, b;
+            b = "";
+            Console.WriteLine("Введите слово ");
+            s = Console.ReadLine();
+            s = s.ToLower();
+            for (int i = s.Length - 1; i>=0; i--)
+            {
+                b += s[i];
+            }
+            if(s==b)
+            {
+                Console.WriteLine("Is polindrom");
+            }
+            else
+            {
+                Console.WriteLine("Is not polindrom");
+            }
+        }
+    }
+}
